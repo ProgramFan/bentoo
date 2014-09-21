b87260faa3328bdcced30b073d96c50221858fcc
Yang Zhang <yang_zhang@iapcm.ac.cn>
2014-09-21 11:03:10 +0800

    添加 analyser 和 collector 基本代码

---
+++ analyser.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ collector.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+#
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
