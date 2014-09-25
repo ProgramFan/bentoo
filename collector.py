81ce7a865e9a83e44fa06a1db9e820521bb95ce1
Yang Zhang <yang_zhang@iapcm.ac.cn>
2014-09-25 19:36:40 +0800

    实现 collector JASMIN log 文件解析器

--- collector.py
+++ collector.py
@@ -1,8 +1,192 @@
 #!/usr/bin/env python2.7
 #
+# Tool to collect test results and save them into a database
+#
+
+import os
+import sys
+import re
+import string
+from collections import OrderedDict
+
+
+def parse_jasminlog(fn):
+    '''parse_jasminlog - jasmin time manager log parser
+
+    This function parses jasmin timer manager performance reports. It
+    splits detected jasmin timer logs into several measures and assign
+    them unique names.
+
+    Assume the following log::
+
+      ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+      TOTAL
+      WALLCLOCK TIME
+      ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+      Timer Name                                       Proc: 0     Max
+      alg::NumericalIntegratorComponent::computing()   3.55 (100%) 7.3(90%)
+      ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    Examples
+    --------
+
+    The use of this function is fairly straight forward:
+    >>> info = parse_jasminlog("demo_jasminlog.txt")
+    >>> info[-1]["data"][0]["timer_name"]
+    TOTAL_RUN_TIME
+    >>> info[-1]["data"][0]["summed"]
+    5.14626
+
+    '''
+
+    def tokenlize(s):
+        '''Convert string into a valid lower case pythonic token'''
+        invalid_chars = ":-+*/#\n"
+        trans_table = string.maketrans(invalid_chars, " "*len(invalid_chars))
+        return "_".join(map(string.lower, s.translate(trans_table).split()))
+
+    avail_types = {
+        "timer_name": str,
+        "proc_0": float,
+        "proc_0_percent": float,
+        "summed": float,
+        "summed_percent": float,
+        "max": float,
+        "max_percent": float,
+        "proc": int
+    }
+    content = file(fn, "r").read()
+    logtbl_ptn = re.compile("^\+{80}$(?P<name>.*?)^\+{80}$"
+                            + ".*?(?P<header>^.*?$)"
+                            + "(?P<content>.*?)^\+{80}$", re.M+re.S)
+    result = []
+    for match in logtbl_ptn.finditer(content):
+        log_table = match.groupdict()
+        table_name = tokenlize(log_table["name"])
+        # We only handle table "TOTAL WALLCLOCK TIME"
+        if table_name != "total_wallclock_time":
+            continue
+        # Extract table header
+        header_ptn = re.compile(r"(Timer Name|Proc: \d+|Summed|Proc|Max)")
+        header = map(tokenlize, header_ptn.findall(log_table["header"]))
+        # Parse table rows
+        table_contents = []
+        for ln in log_table["content"].strip().split("\n"):
+            rec_ptn = re.compile(r"^\s*(TOTAL RUN TIME:|\S+)\s*(.*)$")
+            tl, tr = rec_ptn.search(ln).groups()
+            timer_name = tl.strip()
+            if timer_name == "TOTAL RUN TIME:":
+                timer_name = "TOTAL_RUN_TIME"
+            timer_rec = {"timer_name": avail_types["timer_name"](timer_name)}
+            flt_ptn = r"[-+]?(?:\d+(?:\.\d*)?)(?:[Ee][-+]?\d+)?"
+            seg_ptn = re.compile(r"({0})\s*(\({0}%\))?".format(flt_ptn))
+            for i, seg in enumerate(seg_ptn.finditer(tr)):
+                # Example: 99.9938 (97%)
+                a, b = seg.groups()
+                cn = header[i + 1]
+                timer_rec[cn] = avail_types[cn](a)
+                if b:
+                    pn = "{0}_percent".format(cn)
+                    timer_rec[pn] = avail_types[pn](b[1:-2]) * 0.01
+            table_contents.append(timer_rec)
+        # Fix table header when there are XX% records
+        for k in table_contents[0].iterkeys():
+            if k not in header:
+                header.append(k)
+        # Make final result
+        types = {x: avail_types[x] for x in header}
+        table = {"columns": header, "column_types": types,
+                 "data": table_contents}
+        result.append(table)
+    return result
+
+
+def parse_jasmin4log(fn):
+    '''parse_jasmin4log - jasmin 4.0 time manager log parser
+
+    This function parses jasmin 4.0 timer manager performance reports. It
+    splits detected jasmin timer logs into several measures and assign
+    them unique names.
+
+    Assume the following log::
+
+    *************************** TIME STATISTICS  ****************************
+    -------------------------------------------------------------------------
+                                                       Name           MaxTime
+    -------------------------------------------------------------------------
+                                             TOTAL RUN TIME   0.9065(100.00%)
+    algs::SimpleHierarchyTimeIntegrator::advanceHierarchy()    0.8624(95.14%)
+    -------------------------------------------------------------------------
+
+    Examples
+    --------
+
+    The use of this function is fairly straight forward:
+    >>> info = parse_jasmin4log("demo_jasmin4log.txt")
+    >>> info[-1]["data"][0]["Name"]
+    TOTAL RUN TIME
+    >>> info[-1]["data"][0]["Accesses"]
+    1
+
+    '''
+
+    avail_types = {
+        "Name": str,
+        "MaxTime": float,
+        "MaxTime_percent": float,
+        "AvgTime": float,
+        "AvgTime_percent": float,
+        "LoadBalance": float,
+        "Accesses": int,
+        "Overhead": float
+    }
+    content = file(fn, "r").read()
+    logtbl_ptn = re.compile(r"^\*+ (?P<name>.*?) \*+$\n-{10,}\n"
+                            + r"^(?P<header>^.*?$)\n-{10,}\n"
+                            + r"(?P<content>.*?)^-{10,}\n", re.M+re.S)
+    result = []
+    for match in logtbl_ptn.finditer(content):
+        log_table = match.groupdict()
+        table_name = log_table["name"]
+        # Extract table header
+        header = log_table["header"].split()
+        # Parse table rows
+        table_contents = []
+        for ln in log_table["content"].strip().split("\n"):
+            timer_rec = {}
+            seg_ptn = re.compile(r"(TOTAL RUN TIME|\S+)")
+            for i, seg in enumerate(seg_ptn.finditer(ln)):
+                cn = header[i]
+                val = seg.group(1)
+                flt_ptn = r"[-+]?(?:\d+(?:\.\d*)?)(?:[Ee][-+]?\d+)?|[+-]?nan"
+                m = re.match(r"({0})\(({0})%\)".format(flt_ptn), val)
+                if m:
+                    pn = "{0}_percent".format(cn)
+                    a, b = map(float, [m.group(1), m.group(2)])
+                    b = b * 0.01
+                    timer_rec[cn], timer_rec[pn] = a, b
+                    continue
+                m = re.match(r"({0})%".format(flt_ptn), val)
+                if m:
+                    timer_rec[cn] = float(m.group(1)) * 0.01
+                    continue
+                timer_rec[cn] = avail_types[cn](val)
+            table_contents.append(timer_rec)
+        # Fix table header when there are XX% records
+        for k in table_contents[0].iterkeys():
+            if k not in header:
+                header.append(k)
+        # Make final result
+        types = {x: avail_types[x] for x in header}
+        table = {"columns": header, "column_types": types,
+                 "data": table_contents}
+        result.append(table)
+    return result
+
 
 def main():
-    pass
+    content = parse_jasmin4log(sys.argv[1])
+    print content
 
 
 if __name__ == "__main__":
