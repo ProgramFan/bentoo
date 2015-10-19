4898dd8c9eba60427493f7e0ea4935d95528eb2b
Yang Zhang <yang_zhang@iapcm.ac.cn>
2015-10-19 08:59:07 +0800

    Tag v0.7 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="bento",
     description="Benchmarking tools",
-    version="0.5",
+    version="0.7",
     packages=find_packages(),
     scripts=["scripts/generator.py", "scripts/runner.py",
              "scripts/collector.py", "scripts/analyser.py"],
