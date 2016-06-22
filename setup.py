e93e53624702606a73769189163111ba62947702
Yang Zhang <yang_zhang@iapcm.ac.cn>
2016-06-22 16:38:42 +0800

    Publsh bentoo-calltree-analyser

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     scripts=["scripts/bentoo-generator.py", "scripts/bentoo-runner.py",
              "scripts/bentoo-collector.py", "scripts/bentoo-analyser.py",
              "scripts/bentoo-metric.py", "scripts/bentoo-quickstart.py",
-             "scripts/bentoo-calltree.py", "scripts/bentoo-merge.py"],
+             "scripts/bentoo-calltree.py", "scripts/bentoo-merge.py",
+             "scripts/bentoo-calltree-analyser.py"],
     package_data={
         '': ['*.adoc', '*.rst', '*.md']
     },
