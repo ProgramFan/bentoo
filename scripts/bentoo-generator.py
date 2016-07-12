a8967e991f4588b3ec5b3562950f2b42fffd689b
Yang Zhang <yang_zhang@iapcm.ac.cn>
2016-07-12 19:40:00 +0800

    Add yaml reader to bentoo-generator

--- scripts/bentoo-generator.py
+++ scripts/bentoo-generator.py
@@ -13,6 +13,54 @@ import string
 
 from collections import OrderedDict
 
+try:
+    import yaml
+
+    def dict_representer(dumper, data):
+        return dumper.represent_dict(data.iteritems())
+
+    def dict_constructor(loader, node):
+        return OrderedDict(loader.construct_pairs(node))
+
+    yaml.add_representer(OrderedDict, dict_representer)
+    yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+                         dict_constructor)
+
+    def load(fileobj, *args, **kwargs):
+        return yaml.load(fileobj, *args, **kwargs)
+
+    def loads(string, *args, **kwargs):
+        return yaml.load(string, *args, **kwargs)
+
+    def dump(data, fileobj, *args, **kwargs):
+        fileobj.write(yaml.dump(data), *args, **kwargs)
+
+    def dumps(data, *args, **kwargs):
+        return yaml.dump(data, *args, **kwargs)
+
+except ImportError:
+    import json
+
+    def load(fileobj, *args, **kwargs):
+        return json.load(fileobj,
+                         object_pairs_hook=OrderedDict,
+                         *args,
+                         **kwargs)
+
+    def loads(string, *args, **kwargs):
+        return json.loads(string,
+                          object_pairs_hook=collections.OrderedDict,
+                          *args,
+                          **kwargs)
+
+    def dump(data, fileobj, *args, **kwargs):
+        kwargs["indent"] = 2
+        json.dump(data, fileobj, *args, **kwargs)
+
+    def dumps(data, *args, **kwargs):
+        kwargs["indent"] = 2
+        return json.dumps(data, *args, **kwargs)
+
 
 def parse_json(fn):
     '''Parse json object
@@ -46,7 +94,7 @@ def parse_json(fn):
         return result
     content = file(fn).read()
     content = re.sub(r"//.*", "", content)
-    return ununicodify(json.loads(content, object_pairs_hook=OrderedDict))
+    return ununicodify(loads(content))
 
 
 class SimpleVectorGenerator:
