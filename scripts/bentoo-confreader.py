2ff9f066c1402b11d672a7b8ba2679ecab9c1dfb
Yang Zhang <yang_zhang@iapcm.ac.cn>
2016-07-13 01:20:15 +0800

    Add an comment-preserving conf reader

--- scripts/bentoo-confreader.py
+++ scripts/bentoo-confreader.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 #
-
 '''
 bentoo-confreader - Read and convert config files
 
@@ -14,52 +13,68 @@ import argparse
 import collections
 
 try:
-    import yaml
-
-    def dict_representer(dumper, data):
-        return dumper.represent_dict(data.iteritems())
-
-    def dict_constructor(loader, node):
-        return collections.OrderedDict(loader.construct_pairs(node))
-
-    yaml.add_representer(collections.OrderedDict, dict_representer)
-    yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-                         dict_constructor)
+    import ruamel.yaml
 
     def load(fileobj, *args, **kwargs):
-        return yaml.load(fileobj, *args, **kwargs)
+        return ruamel.yaml.load(fileobj, ruamel.yaml.RoundTripLoader)
 
     def loads(string, *args, **kwargs):
-        return yaml.load(string, *args, **kwargs)
+        return ruamel.yaml.load(fileobj, ruamel.yaml.RoundTripLoader)
 
     def dump(data, fileobj, *args, **kwargs):
-        fileobj.write(yaml.dump(data), *args, **kwargs)
+        ruamel.yaml.dump(data, fileobj, Dumper=ruamel.yaml.RoundTripDumper)
 
     def dumps(data, *args, **kwargs):
-        return yaml.dump(data, *args, **kwargs)
+        return ruamel.yaml.dump(data, Dumper=ruamel.yaml.RoundTripDumper)
 
 except ImportError:
-    import json
+    try:
+        import yaml
 
-    def load(fileobj, *args, **kwargs):
-        return json.load(fileobj,
-                         object_pairs_hook=collections.OrderedDict,
-                         *args,
-                         **kwargs)
+        def dict_representer(dumper, data):
+            return dumper.represent_dict(data.iteritems())
 
-    def loads(string, *args, **kwargs):
-        return json.loads(string,
-                          object_pairs_hook=collections.OrderedDict,
-                          *args,
-                          **kwargs)
+        def dict_constructor(loader, node):
+            return collections.OrderedDict(loader.construct_pairs(node))
 
-    def dump(data, fileobj, *args, **kwargs):
-        kwargs["indent"] = 2
-        json.dump(data, fileobj, *args, **kwargs)
+        yaml.add_representer(collections.OrderedDict, dict_representer)
+        yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+                             dict_constructor)
 
-    def dumps(data, *args, **kwargs):
-        kwargs["indent"] = 2
-        return json.dumps(data, *args, **kwargs)
+        def load(fileobj, *args, **kwargs):
+            return yaml.safe_load(fileobj, *args, **kwargs)
+
+        def loads(string, *args, **kwargs):
+            return yaml.safe_load(string, *args, **kwargs)
+
+        def dump(data, fileobj, *args, **kwargs):
+            yaml.safe_dump(data, fileobj, *args, **kwargs)
+
+        def dumps(data, *args, **kwargs):
+            return yaml.safe_dump(data, *args, **kwargs)
+
+    except ImportError:
+        import json
+
+        def load(fileobj, *args, **kwargs):
+            return json.load(fileobj,
+                             object_pairs_hook=collections.OrderedDict,
+                             *args,
+                             **kwargs)
+
+        def loads(string, *args, **kwargs):
+            return json.loads(string,
+                              object_pairs_hook=collections.OrderedDict,
+                              *args,
+                              **kwargs)
+
+        def dump(data, fileobj, *args, **kwargs):
+            kwargs["indent"] = 2
+            json.dump(data, fileobj, *args, **kwargs)
+
+        def dumps(data, *args, **kwargs):
+            kwargs["indent"] = 2
+            return json.dumps(data, *args, **kwargs)
 
 
 def main():
