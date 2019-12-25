de53f1598a05b6c2573f546625a69a525844429a
Yang Zhang <yang_zhang@iapcm.ac.cn>
2019-12-25 10:41:17 +0800

    Fix more bugs

--- bentoo/common/utils.py
+++ bentoo/common/utils.py
@@ -6,16 +6,16 @@ import os
 
 
 def replace_template(template, varvalues):
-    return string.Template(repr(template)).safe_substitute(varvalues)
+    return string.Template(str(template)).safe_substitute(varvalues)
 
 
 def safe_eval(expr):
     try:
-        result = eval(repr(expr))
+        result = eval(str(expr))
     except ZeroDivisionError:
         result = 0
     except Exception:
-        result = repr(expr)
+        result = str(expr)
     return result
 
 
