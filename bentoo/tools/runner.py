c64b10c34d1c9946ba70ad99950a8af9e06c2585
LinBob <87francium@gmail.com>
2020-07-23 14:27:10 +0800

    fix bugs

--- bentoo/tools/runner.py
+++ bentoo/tools/runner.py
@@ -635,7 +635,7 @@ class PbsLauncher(object):
             "iface": "",
             "queue": "",
             "jobname": os.path.basename(spec["cmd"][0]),
-            "ppn": procs_per_node * run.get("tasks_per_proc",1),
+            "ppn": str(run["procs_per_node"] * run.get("tasks_per_proc",1)),
             "timeout": "",
         }
         if self.args["iface"]:
